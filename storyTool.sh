029f2320f93726f0d2c4361df33f472e002ca875
Ali <alikohere@gmail.com>
2024-09-17 17:49:28 +0000

    Now fresh installs grabs the last 2 realeses to avoid any mistakes

--- storyTool.sh
+++ storyTool.sh
@@ -21,6 +21,42 @@ mainMenu ()
     echo "q Quit"
 }
 
+
+installMenu() {
+    latestConsVersion=$(curl -s https://api.github.com/repos/piplabs/story/releases/latest | grep tag_name | cut -d\" -f4)
+    secondChoiceVersion=$(curl -s "https://api.github.com/repos/piplabs/story/tags" | grep -oP '"name": "\K[^"]+' | sed -n '2p')
+    echo -e "\033[31mNote: Sometimes installing the latest version can break your node\nCheck on Discord if you're not sure https://discord.com/invite/storyprotocol\033[0m"
+    PS3="Select an option: "
+    select subopt in "Install version $latestConsVersion - Latest" "Install version $secondChoiceVersion" "Back"; do
+        case $subopt in
+            "Install version $latestConsVersion - Latest")
+                echo "Getting Story Consesus..."
+                wget -qO story.tar.gz $(curl -s https://api.github.com/repos/piplabs/story/releases/latest | grep 'body' | grep -Eo 'https?://[^ ]+story-linux-amd64[^ ]+' | sed 's/......$//')
+                echo "Extracting and configuring Story..."
+                tar xf story.tar.gz
+                cp story*/story /bin
+                rm -rf story*/ | rm story.tar.gz
+                freshInstallSecondPart
+                ;;
+            "Install version $secondChoiceVersion")
+                echo "Getting Story Consesus..."
+                wget -qO story.tar.gz $(curl -s https://github.com/piplabs/story/releases/tag/$secondChoiceVersion | grep binaries | grep -Eo 'https?://[^ ]+story-linux-amd64[^ ]+' | sed 's/.$//')
+                echo "Extracting and configuring Story..."
+                tar xf story.tar.gz
+                cp story*/story /bin
+                rm -rf story*/ | rm story.tar.gz
+                freshInstallSecondPart
+                ;;
+            "Back")
+                break
+                ;;
+            *)
+                echo "Invalid option $REPLY"
+                ;;
+        esac
+    done
+}
+
 installStoryGeth () # Story Geth install function
 {
     echo "Getting Story Geth..."
@@ -39,6 +75,20 @@ installStoryConsensus () # Story Consensus install function
     tar xf story.tar.gz
     cp story*/story /bin
     rm -rf story*/ | rm story.tar.gz
+}
+
+freshInstallSecondPart ()
+{
+    installStoryGeth
+    echo "Please enter your moniker"
+    read varname
+    story init --network iliad --moniker $varname
+    createStoryConsensusServiceFile
+    createStoryGethServiceFile
+    echo "Adding Peers"
+    sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$(curl -sS https://story-rpc.oreonserv.com/net_info | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr)"' | awk -F ':' '{print $1":"$(NF)}' | paste -sd, -)\"/" $HOME/.story/story/config/config.toml
+    echo "Restarting the services"
+    sudo systemctl restart story story-geth
 }
 
 createStoryConsensusServiceFile ()
@@ -99,17 +149,7 @@ do
     echo
     case "$CHOICE" in
         "1") # Install Story node
-            installStoryConsensus
-            installStoryGeth
-            echo "Please enter your moniker"
-            read varname
-            story init --network iliad --moniker $varname
-            createStoryConsensusServiceFile
-            createStoryGethServiceFile
-            echo "Adding Peers"
-            sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$(curl -sS https://story-rpc.mandragora.io/net_info | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr)"' | awk -F ':' '{print $1":"$(NF)}' | paste -sd, -)\"/" $HOME/.story/story/config/config.toml
-            echo "Restarting the services"
-            sudo systemctl restart story story-geth
+            installMenu
             echo
             ;;
         "2") # Update Story Consensus
@@ -123,11 +163,11 @@ do
             case $yn in 
                 [yY] ) echo updating...;
                         echo "stopping the services"
-                        sudo systemctl stop story story-geth
+                        sudo systemctl stop story
                         rm /bin/story
                         installStoryConsensus
                         echo "Starting the services"
-                        sudo systemctl start story story-geth
+                        sudo systemctl start story
                         echo "Done!"
                         break;;
                 [nN] ) echo exiting...;
@@ -147,11 +187,11 @@ do
             case $yn in 
                 [yY] ) echo Updating...;
                     echo "stopping the services"
-                    sudo systemctl stop story story-geth
+                    sudo systemctl stop story-geth
                     rm /bin/geth
                     installStoryGeth
                     echo "Starting the services"
-                    sudo systemctl start story story-geth
+                    sudo systemctl start story-geth
                     echo "Done!"
                     break;;
                 [nN] ) echo exiting...;
@@ -211,10 +251,10 @@ do
             mv $HOME/.story/story/data/priv_validator_state.json $HOME/.story/priv_validator_state.json.backup
             rm -rf ~/.story/story/data
             rm -rf ~/.story/geth/iliad/geth/chaindata
-            sudo mkdir -p /root/.story/story/data
-            lz4 -d story_snapshot.lz4 | pv | sudo tar xv -C /root/.story/story/
-            sudo mkdir -p /root/.story/geth/iliad/geth/chaindata
-            lz4 -d geth_snapshot.lz4 | pv | sudo tar xv -C /root/.story/geth/iliad/geth/
+            sudo mkdir -p $HOME/.story/story/data
+            lz4 -d story_snapshot.lz4 | pv | sudo tar xv -C $HOME/.story/story/
+            sudo mkdir -p $HOME/.story/geth/iliad/geth/chaindata
+            lz4 -d geth_snapshot.lz4 | pv | sudo tar xv -C $HOME/.story/geth/iliad/geth/
             mv $HOME/.story/priv_validator_state.json.backup $HOME/.story/story/data/priv_validator_state.json
             echo "Starting the Story and Story Geth services"
             systemctl start story story-geth
